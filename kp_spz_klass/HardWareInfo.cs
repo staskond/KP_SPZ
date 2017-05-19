61e00ec10cd68820b20d63a6de71420bcadc4b65
staskond <staskond@gmail.com>
2017-05-19 18:56:18 +0300

    added the class HardWareInfo.cs

---
+++ kp_spz_klass/HardWareInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kp_spz_klass
+{
+    [Serializable]
+    class HardWareInfo
+    {
+        public string HDDserialNumber { get; set; }
+        public string ProcessorName { get; set; }
+        public string ProcessorID { get; set; }
+        public string VideoControllerId { get; set; }
+        public string BaseGoardSerialNumber { get; set; }
+
+        public HardWareInfo(string HDDserialNumber,
+            string ProcessorName,
+            string ProcessorID,
+            string VideoControllerId,
+            string BaseGoardSerialNumber)
+        {
+            this.HDDserialNumber = HDDserialNumber;
+            this.ProcessorName = ProcessorName;
+            this.ProcessorID = ProcessorID;
+            this.VideoControllerId = VideoControllerId;
+            this.BaseGoardSerialNumber = BaseGoardSerialNumber;
+        }
+    }
+}
