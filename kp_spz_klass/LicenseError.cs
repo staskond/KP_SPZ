2da7297945106ff7336be7e95145512fb6dc8c2c
staskond <staskond@gmail.com>
2017-05-21 06:31:16 +0300

    поправил форму

---
+++ kp_spz_klass/LicenseError.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kp_spz_klass
+{
+    public partial class LicenseError : Form
+    {
+        public LicenseError()
+        {
+            InitializeComponent();
+            closeApp.Click += (object sender, EventArgs e) =>
+            {
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            };
+        }
+        
+    }
+}
